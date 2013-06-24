b68473a7a0cce8e171f1444f6775ded7de34f331
mm <info@decentweb.de>
2013-06-24 23:34:13 +0200

    closure callbacks

--- api/index.php
+++ api/index.php
@@ -12,48 +12,19 @@ $data 	 = $request->getData();
 // $valid_data = (isset($data) && is_array($data) && !empty($data));
 
 $routes = array(
-	"POST   /{$modelName}"				=> 'create_model',
-	"PUT    /{$modelName}/(?<id>\d+)"		=> 'update_model',
-	"PATCH  /{$modelName}/(?<id>\d+)"		=> 'update_model',
-	"DELETE /{$modelName}/(?<id>\d+)"		=> 'delete_model',
-	"GET    /{$modelName}/(?<id>\d+)"		=> function($id) use ($model) {return $model->get($id);},
-	"GET    /{$modelName}"		=> function() use ($model) { return $model->getAll();}
+	"POST   /{$modelName}"				=> function() use ($model) {return $model->create($id);},
+	"PUT    /{$modelName}/(?<id>\d+)"	=> function($id) use ($model) {return $model->update($id);},
+	"PATCH  /{$modelName}/(?<id>\d+)"	=> function($id) use ($model) {return $model->update($id);},
+	"DELETE /{$modelName}/(?<id>\d+)"	=> function($id) use ($model) {return $model->delete($id);},
+	"GET    /{$modelName}/(?<id>\d+)"	=> function($id) use ($model) {return $model->get($id);},
+	"GET    /{$modelName}"				=> function() use ($model) { return $model->getAll();}
 );
 
-// if ($model == 'note')
+
 $result = Router::run($routes);
-// else $result = '';
+
 echo json_encode($result);
 
-if (isset($log)) debug($log);
-function create_model($modelName){
-	return $model->create($data);
-}
-
-function update_model($modelName, $id){
-	return $model->update($data);
-}
-
-function delete_model($id){
-	return $model->delete($id);
-}
-
-function get_model($id){
-	echo "get";
-	return $model->get($id);
-}
-
-function getall_models(){
-	return $model->getAll();
-}
-
-
-// var_dump($_POST);
-// var_dump($request::getHeaders());
-// echo $req::getHeaders('Content-Type');
-// echo $req::getHeaders('Accept-Encoding');
-//var_dump($req->getData());
-\PhpConsole::debug('hallo');
 
 function debug($message, $tags = 'debug') {
 	if ($GLOBALS['debug'] === true){
